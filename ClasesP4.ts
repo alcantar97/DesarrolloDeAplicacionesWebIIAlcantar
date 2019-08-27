5805025a93abc979f99267b3e144323b3243d92c
Sinaí Alcantar <alcantarsina@gmail.com>
2019-08-27 01:33:41 -0600

    Herencia en TypeScript

--- ClasesP4.ts
+++ ClasesP4.ts
@@ -7,6 +7,21 @@ class Person{
         console.log("Hi, I am "+this.name+"!");
     }
 }
+
+class Friend extends Person{
+    yearsKnown:number;
+    constructor(name:string, yearsKnown:number){
+        super(name);
+        this.yearsKnown= yearsKnown;
+    }
+    timeKnown(){
+        console.log("We have been friends for "+this.yearsKnown+" years.")
+    }
+}
 let personA= new Person("Sally");
 
 personA.introduceSelf();
+
+let friendA=new Friend("Jacob",6);
+friendA.introduceSelf();
+friendA.timeKnown();
