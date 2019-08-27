77032f1a0bbcb5ec1c4990a359e63c0b22034237
Sinaí Alcantar <alcantarsina@gmail.com>
2019-08-27 01:54:49 -0600

    La necesidad de genéricos

---
+++ EjerciciosPrimerLibroTypescript/GenericosP5.ts
@@ -0,0 +1,21 @@
+function randomIntElem(theArray:number[]):number{
+    let randomIndex=Math.floor(Math.random()*theArray.length);
+    return theArray[randomIndex];
+}
+function randomStrElem(theArray:string[]):string{
+    let randomIndex = Math.floor(Math.random()*theArray.length);
+    return theArray[randomIndex];
+}
+function randomElem<T>(theArray: T[]):T{
+    let randomIndex = Math.floor(Math.random()*theArray.length);
+    return theArray[randomIndex];
+}
+
+let positions:number[]=[103,458,472,458];
+//let randomPosition:number=randomIntElem(positions);
+let randomPosition:number=randomElem(positions);
+
+let colors:string[]=['violet','indigo','blue','green'];
+//let randomColor:string = randomStrElem(colors);
+let randomColor:string = randomElem(colors);
+
