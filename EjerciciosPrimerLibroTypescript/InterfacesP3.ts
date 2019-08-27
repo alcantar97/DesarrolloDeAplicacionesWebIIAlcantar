69da30811d383ae464e4be41a7926013b5508153
Sinaí Alcantar <alcantarsina@gmail.com>
2019-08-27 01:21:06 -0600

    Propiedades de solo lectura

--- EjerciciosPrimerLibroTypescript/InterfacesP3.ts
+++ EjerciciosPrimerLibroTypescript/InterfacesP3.ts
@@ -39,3 +39,18 @@ let fourLake: Lakes = {
     kalemi: 'Congo',
     bujumbura: 'Burundi'
 }
+
+interface Enemy {
+    readonly size:number,
+    health:number,
+    range:number,
+    readonly damage:number
+}
+
+let tank: Enemy={
+    size:50,
+    health:100,
+    range:60,
+    damage:12
+}
+tank.health= 95;
