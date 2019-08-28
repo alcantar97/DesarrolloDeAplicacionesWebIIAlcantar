422e2fdcaff03ef187e2b6bd003bfb8d123b2afa
Sinaí Alcantar <alcantarsina@gmail.com>
2019-08-28 16:25:43 -0600

    TypeScrypt Libro 2: For in

---
+++ EjerciciosSegundoLibroTypescript/ForIn.js
@@ -0,0 +1,7 @@
+var list = { a: 1, b: 2, c: 3 };
+for (var i in list) {
+    console.log(list[i]);
+}
+for (var i in list) {
+    console.log(i);
+}
---
+++ EjerciciosSegundoLibroTypescript/ForIn.ts
@@ -0,0 +1,8 @@
+let  list = {a: 1, b: 2, c:3};
+for (let i in list){
+    console.log(list[i]);
+}
+
+for (let i in list){
+    console.log(i);
+}
