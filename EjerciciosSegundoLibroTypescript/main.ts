53628c329009a75f1b6169e98353a701640c00ae
Sinaí Alcantar <alcantarsina@gmail.com>
2019-09-01 20:41:51 -0600

    TypeScript libro 2: Generadores

--- EjerciciosSegundoLibroTypescript/main.ts
+++ EjerciciosSegundoLibroTypescript/main.ts
@@ -1,3 +1,4 @@
+/*
 function loadItem(id: number): Promise<{id: number}> {
     return new Promise((resolve)=>{
         console.log('loading item', id);
@@ -21,3 +22,52 @@ Promise.all([loadItem(1),loadItem(2)])
         [item1,item2] = res;
         console.log('done')
     }); 
+    */
+
+/*    
+function* generator(){
+    console.log('Execution started');
+    yield 0;
+    console.log('Execution resumed');
+    yield 1;
+    console.log('Execution resumed');
+}
+var iterator = generator();
+console.log('Starting iteration'); 
+console.log(iterator.next()); 
+console.log(iterator.next()); 
+console.log(iterator.next()); 
+*/
+
+function* generator() {
+    try {
+        yield 'foo';
+        throw Error("Test");
+    }
+    catch(err) {
+        console.log(err.message); // bar!
+    }
+}
+var iterator = generator();
+var foo = iterator.next();
+console.log(foo.value);
+var foo = iterator.next();
+
+
+function getFirstName() {
+    setTimeout(function(){
+        gen.next('alex')
+    }, 1000);
+}
+function getSecondName() {
+    setTimeout(function(){
+        gen.next('perry')
+    }, 1000);
+}
+function *sayHello() {
+    var a = yield getFirstName();
+    var b = yield getSecondName();
+    console.log(a, b); //alex perry
+}
+var gen = sayHello();
+gen.next();
