6b5954a74dacf0516854d8817db2671d57eff3aa
Sinaí Alcantar <alcantarsina@gmail.com>
2019-09-02 00:16:33 -0600

    TypeScript libro 2: Static method decorator; Parameter decorator

--- EjerciciosSegundoLibroTypescript/methoddecorator.ts
+++ EjerciciosSegundoLibroTypescript/methoddecorator.ts
@@ -10,3 +10,27 @@ class MethodDecoratorExample {
     method() {
     }
 }
+function StaticMethodDecorator(
+    target: Function, 
+    propertyKey: string | symbol, 
+    descriptor: TypedPropertyDescriptor<any>
+    ) {
+    console.log("StaticMethodDecorator called on: ", target, propertyKey, descriptor);
+}
+class StaticMethodDecoratorExample {
+    @StaticMethodDecorator
+    static staticMethod() {
+    }
+}
+
+function ParameterDecorator(
+    target: Function, 
+    propertyKey: string | symbol, 
+    parameterIndex: number 
+    ) {
+    console.log("ParameterDecorator called on: ", target, propertyKey, parameterIndex);
+    }
+class ParameterDecoratorExample {
+    method(@ParameterDecorator param1: string, @ParameterDecorator param2: number) {
+    }
+}
