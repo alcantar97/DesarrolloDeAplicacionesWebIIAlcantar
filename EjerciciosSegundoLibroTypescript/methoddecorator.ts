1cf379ee7c9b9c8b06470c78351d4762c102808f
Sinaí Alcantar <alcantarsina@gmail.com>
2019-09-02 00:10:20 -0600

    TypeScrypt libro 2: Method Decorators

---
+++ EjerciciosSegundoLibroTypescript/methoddecorator.ts
@@ -0,0 +1,12 @@
+function MethodDecorator(
+    target: Object, 
+    propertyKey: string, 
+    descriptor: TypedPropertyDescriptor<any>
+    ) {
+    console.log("MethodDecorator called on: ", target, propertyKey, descriptor);
+}
+class MethodDecoratorExample {
+    @MethodDecorator
+    method() {
+    }
+}
